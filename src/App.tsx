5992cc4ca4af2e281987be078b1401aeb0270376
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-07 19:46:17 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router";
 
 
 export function App() {
   return (
     <div className="App">
-      Parham
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutMePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/resume" element={<ResumePage />} />
+      </Routes>
     </div>
   )
 }
