6caaac33cc7555e8bb5a80b0bd5e31a50e21350a
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-09 01:31:29 +0200

    added divs for animation

--- src/components/GraphicDesignCard.tsx
+++ src/components/GraphicDesignCard.tsx
@@ -7,9 +7,12 @@ interface Skill {
 
 export function GraphicDesignCard({ skill }: Skill) {
     return (
-        <div className="cardBody">
-            <div>{skill.image}</div>
-            <p>{skill.name}</p>
+        <div className="outerBody">
+            <div className="borderAnimaion"></div>
+            <div className="cardBody">
+                <div>{skill.image}</div>
+                <p>{skill.name}</p>
+            </div>
         </div>
     )
 }
