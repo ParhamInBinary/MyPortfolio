8fcc5f7eddc260905d3ef93a42929b1bc5921675
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-08 18:12:32 +0200

    corrected link name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,11 @@ export function Navbar() {
     return (
         <header className="navbar">
             <div className="badge">{'<ParhamBerenjianDev />'}</div>
+            
             <div className="nav">
             <NavLink className="link" to="/">Home</NavLink>
             <NavLink className="link" to="/about">About me</NavLink>
-            <NavLink className="link" to="/projects">Project</NavLink>
+            <NavLink className="link" to="/projects">Projects</NavLink>
             <NavLink className="link" to="/resume">Resume</NavLink>
             </div>
         </header>
