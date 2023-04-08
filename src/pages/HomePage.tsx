6e5fab36fd4bcd341133c2cd99f1e7859a5a378b
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-09 00:23:50 +0200

    mapping out skillcards on homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,58 @@
 import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
-import "./styles/HomePage.css";
+import { BackEndCard } from "../components/BackEndCard";
+import { FrontEndCard } from "../components/FrontEndCard";
+import { GraphicDesignCard } from "../components/GraphicDesignCard";
+import { graphicDesignSkills } from "../data/GraphicDesignSkills";
+import { backEndSkills } from "../data/backEndSkills";
+import { frontEndSkills } from "../data/frontEndSkills";
+import "../styles/HomePage.css";
 
 export function HomePage() {
   return (
     <div className="container">
-      <div className="introText">
-        <p>Fullstack developer</p>
-        <span>Parham Berenjian</span>
-        <p>Based in Gothenburg, Sweden</p>
+      <div className="presentation">
+        <div className="profile">
+          <div className="introText">
+            <p>Fullstack developer</p>
+            <span>Parham Berenjian</span>
+            <p>Based in Gothenburg, Sweden</p>
+          </div>
 
+          <div className="picture"></div>
+        </div>
         <div className="logos">
-          <NavLink to='https://github.com/ParhamInBinary'>
+          <NavLink to="https://github.com/ParhamInBinary">
             <div>
-            <FaGithubSquare />
+              <FaGithubSquare />
             </div>
           </NavLink>
 
-          <NavLink to='https://www.linkedin.com/in/parham-berenjian-5a2a26196/'>
+          <NavLink to="https://www.linkedin.com/in/parham-berenjian-5a2a26196/">
             <div>
-            <FaLinkedin />
+              <FaLinkedin />
             </div>
           </NavLink>
         </div>
+      </div>
+      <div className="skillContainer">
+        <div className="CardContainer">
+          {frontEndSkills.map((skill) => (
+            <FrontEndCard key={skill.id} skill={skill} />
+          ))}
+        </div>
+
+        {/* <div className="CardContainer">
+          {backEndSkills.map((skill) => (
+            <BackEndCard key={skill.id} skill={skill} />
+          ))}
+        </div>
+
+        <div className="CardContainer">
+          {graphicDesignSkills.map((skill) => (
+            <GraphicDesignCard key={skill.id} skill={skill} />
+          ))}
+        </div> */}
       </div>
     </div>
   );
