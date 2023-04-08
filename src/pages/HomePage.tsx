c42918562b55f215f7e0854cc1236b5310e93a5a
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-08 18:13:03 +0200

    added content - intro text

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,29 @@
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import "./styles/HomePage.css";
+
 export function HomePage() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <div className="container">
+      <div className="introText">
+        <p>Fullstack developer</p>
+        <span>Parham Berenjian</span>
+        <p>Based in Gothenburg, Sweden</p>
+
+        <div className="logos">
+          <NavLink to='https://github.com/ParhamInBinary'>
+            <div>
+            <FaGithubSquare />
+            </div>
+          </NavLink>
+
+          <NavLink to='https://www.linkedin.com/in/parham-berenjian-5a2a26196/'>
+            <div>
+            <FaLinkedin />
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
