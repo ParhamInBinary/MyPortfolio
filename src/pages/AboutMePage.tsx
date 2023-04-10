f74229a030975a176a7961198f9d6f63d992461c
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-11 01:30:18 +0200

    added text

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -1,3 +1,40 @@
 export function AboutMePage() {
-  return <></>;
+  return (
+    <div className="aboutMeContainer">
+      <h1>Hello,</h1>
+      <div>
+        and welcome to my portfolio! My name is Parham
+        Berenjian, and I'm a fullstack developer with a
+        passion for creating engaging, user-friendly
+        websites and applications. As a positive and driven
+        individual, I'm always looking for new challenges to
+        tackle and ways to improve my skills. I believe in
+        the power of technology to make the world a better
+        place, and I'm committed to using my talents as a
+        developer to make a positive impact. In addition to
+        my work as a fullstack developer, I'm also a musical
+        person who enjoys playing guitar and writing songs
+        in my free time. I find that my love of music often
+        influences my work as a developer, as both require
+        creativity, attention to detail, and a willingness
+        to experiment. Outside of music and coding, I enjoy
+        spending time in nature and learning new things. I'm
+        an avid hiker and love exploring the great outdoors,
+        whether it's a local park or a distant mountain
+        range. I'm also fascinated by history and
+        philosophy, and I'm always eager to dive into a new
+        book or podcast on these subjects. In terms of my
+        professional qualifications, I'm proficient in HTML,
+        CSS, JavaScript, TypeScript, React, MongoDB, and
+        Node.js. I have experience working on a variety of
+        projects, from small websites to complex web
+        applications, and I'm always looking for new
+        challenges to take on. If you're interested in
+        working with me or learning more about my skills and
+        experience, please don't hesitate to get in touch.
+        I'm excited to hear from you and discuss how I can
+        help bring your ideas to life!
+      </div>
+    </div>
+  );
 }
