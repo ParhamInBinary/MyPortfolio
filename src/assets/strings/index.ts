0fa55e7dd551cb48d52a3068ab316e6a47a066d3
ParhamInBinart <parham.berenjian@gmail.com>
2023-11-30 00:47:16 +0100

    feat: strings

---
+++ src/assets/strings/index.ts
@@ -0,0 +1,13 @@
+import aboutMeStrings from './aboutMe.json';
+import contactStrings from './contact.json';
+import homeStrings from './home.json';
+import projectsStrings from './projects.json';
+import resumeStrings from './resume.json';
+
+export {
+  aboutMeStrings,
+  contactStrings,
+  homeStrings,
+  projectsStrings,
+  resumeStrings,
+};
